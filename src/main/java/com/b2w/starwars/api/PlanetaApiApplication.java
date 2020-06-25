64be2d56d8c150a03e0b95508120585e3c51f837
Tiago Lopes <tiagomflopes@poli.ufrj.br>
2020-06-25 19:11:52 -0300

    Criação do Repositório GIT

---
+++ src/main/java/com/b2w/starwars/api/PlanetaApiApplication.java
@@ -0,0 +1,13 @@
+package com.b2w.starwars.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetaApiApplication.class, args);
+	}
+
+}
